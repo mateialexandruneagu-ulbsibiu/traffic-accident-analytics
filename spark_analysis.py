5b66af46db64a14d9b13ab5bb8739526618f22df
Matei Neagu <mateialexandru.neagu@ulbsibiu.ro>
2026-05-31 23:08:47 +0300

    Add analysis for state accidents and weather conditions

--- spark_analysis.py
+++ spark_analysis.py
@@ -12,3 +12,17 @@ spark_df = spark.read.csv(
 )
 
 spark_df.printSchema()
+
+state_accidents = spark_df.groupBy('State') \
+    .agg(count('*').alias('Accident_Count')) \
+    .orderBy('Accident_Count', ascending=False)
+
+state_accidents.show(10)
+
+weather_analysis = spark_df.groupBy('Weather_Condition') \
+    .agg(avg('Severity').alias('Average_Severity')) \
+    .orderBy('Average_Severity', ascending=False)
+
+weather_analysis.show(10)
+
+spark.stop()
