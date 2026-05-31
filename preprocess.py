8a4515ce40db3e013f7316360525a826fe8e1bb7
Matei Neagu <mateialexandru.neagu@ulbsibiu.ro>
2026-05-31 22:45:39 +0300

    Enhance data preprocessing with datetime conversion
    Convert 'Start_Time' to datetime and extract hour, day, month, and year. Select specific columns and save the cleaned dataset to a CSV file.

--- preprocess.py
+++ preprocess.py
@@ -17,3 +17,31 @@ important_columns = [
 
 for col in important_columns:
     df[col].fillna(df[col].median() if df[col].dtype != 'O' else 'Unknown', inplace=True)
+df['Start_Time'] = pd.to_datetime(df['Start_Time'])
+
+df['Hour'] = df['Start_Time'].dt.hour
+df['Day'] = df['Start_Time'].dt.day_name()
+df['Month'] = df['Start_Time'].dt.month_name()
+
+df['Year'] = df['Start_Time'].dt.year
+
+selected_columns = [
+    'Severity',
+    'State',
+    'City',
+    'Weather_Condition',
+    'Visibility(mi)',
+    'Temperature(F)',
+    'Humidity(%)',
+    'Wind_Speed(mph)',
+    'Hour',
+    'Day',
+    'Month'
+]
+
+cleaned_df = df[selected_columns]
+
+cleaned_df.to_csv('data/processed/cleaned_accidents.csv', index=False)
+
+print("Cleaned dataset saved successfully.")
+print("Final Shape:", cleaned_df.shape)
